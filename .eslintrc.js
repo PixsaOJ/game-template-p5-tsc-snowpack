4578dfd2527cd4674e0d8944ccf7e93e02d1b507
PixsaOJ <sabasaba101010@gmail.com>
2021-09-02 02:28:44 +0400

    more configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,28 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    rules: {
-        'semi': ['error', 'always'],
-        'no-console': 2,
-        'quotes': ['error', 'single'],
-        'curly': 0,
-        'brace-style': ['error', 'stroustrup'],
-        'indent': ['error', 4],
-        'eol-last': ['error', 'never'],
-        '@typescript-eslint/no-unused-vars': 2
-    }
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: [
+		'@typescript-eslint',
+	],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	env: {
+		'shared-node-browser': true
+	},
+	rules: {
+		'@typescript-eslint/no-explicit-any': 'off',
+		'semi': ['error', 'always'],
+		'no-console': 2,
+		'quotes': ['error', 'single'],
+		'curly': 0,
+		'brace-style': ['error', 'stroustrup'],
+		'eol-last': ['error', 'never'],
+		'@typescript-eslint/no-unused-vars': 2,
+		'indent': [
+			'error',
+			'tab'
+		]
+	}
 };
--- src/Sketch.ts
+++ src/Sketch.ts
@@ -1,52 +1,60 @@
 import Matter, {
-    Body as BodyType, Engine as EngineType,
-    World as WorldType
+	Body as BodyType,
+	Engine as EngineType,
+	World as WorldType
 } from 'matter-js';
 import p5 from 'p5';
-const { Bodies, Engine, World } = Matter;
+const { Bodies, Engine, World, Composite } = Matter;
 
 
 let engine: EngineType;
 let world: WorldType;
-let box: BodyType;
+let box: BodyType | null;
 const boxy: any = {
-    x: 300,
-    y: 200,
-    w: 80,
+	x: 300,
+	y: 200,
+	w: 80,
 };
 
 const config = {
-    FPS: 120
+	FPS: 120
 };
 
 /**
  * @param {p5} p
  */
-export default (p: p5) : void => {
-    p.setup = () => {
-    // Define your initial environment props & other stuff here
-        p.createCanvas(800, 600);
-        engine = Engine.create();
-        world = engine.world;
-        box = Bodies.rectangle(200, 200, 80, 80);
-        World.add(world, box);
+export default (p: p5): void => {
+	p.setup = () => {
+		p.createCanvas(800, 600);
+		engine = Engine.create();
+		world = engine.world;
+		box = Bodies.rectangle(200, 200, 80, 80);
+		World.add(world, box);
 
-        p.frameRate(config.FPS);
-    };
+		p.frameRate(config.FPS);
+	};
 
-    p.draw = () => {
-    // Define render logic for your sketch here
-        p.background(0);
-        Engine.update(
-            engine,
-            p.deltaTime
-        );
-        p.rect(box.position.x, box.position.y, 80, 80);
-        p.fill(255);
+	p.draw = () => {
+		p.background(0);
+		Engine.update(
+			engine,
+			p.deltaTime
+		);
 
-        p.rect(boxy.x, boxy.y, boxy.w, boxy.w);
+		p.fill(255);
 
-        p.text('Frame Count with frameRate ' + 
-        p.int(p.frameRate()), 100, 100);
-    };
+		if (box) {
+			p.rect(box.position.x, box.position.y, 80, 80);
+			if (box.position.y > p.height + 200) {
+				Composite.remove(world, box);
+				box = null;
+			}
+		}
+
+		p.rect(boxy.x, boxy.y, boxy.w, boxy.w);
+
+
+		p.text('Frame Count with frameRate ' +
+			p.int(p.frameRate()), 100, 100);
+	};
 };
